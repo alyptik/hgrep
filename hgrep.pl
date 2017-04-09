534d4d477461db4a32c545650a2541f22dafa694
alyptik <alyptik@protonmail.com>
2017-04-08 22:27:32 -1000

    Add -n switch to control # of matches to print

--- hgrep.pl
+++ hgrep.pl
@@ -4,25 +4,22 @@ use strict;
 use warnings;
 use File::Grep qw( fgrep fmap fdo );
 use File::Find ();
-
 # Set the variable $File::Find::dont_use_nlink if you're using AFS,
 # since AFS cheats.
-
 # for the convenience of &wanted calls, including -eval statements:
 use vars qw/*name *dir *prune/;
 *name   = *File::Find::name;
 *dir    = *File::Find::dir;
 *prune  = *File::Find::prune;
-
 sub wanted;
 
 my $usage = "Usage: hgrep <query>\n";
 my $version = v0.1;
 my @query = @ARGV;
 my @files;
+my $num;
 # my @matches;
 # my $total;
-
 
 # Parse command line switches
 {
@@ -33,17 +30,13 @@ my @files;
 	printf "hgrep %vd\n", $version if $v;
 	map { $_ = qr/$_/ } @query if $r;
 	map { $_ = quotemeta $_ } @query unless $r;
+	$num = $n // 10;
 }
 
-print join(" ", @query)."\n";
+print "# of matches that will be printed: ".$num."\n"."Queries: \"".join("\" \"", @query)."\"\n";
 
 # Traverse desired filesystems
 File::Find::find({wanted => \&wanted}, '/usr/include');
-
-# print join " ", @files[100..150];
-# $total = fgrep { chomp && push @matches, $_ if m/$query[0]/ } @files[100..200];
-# print join("\n", @matches) . "\n" . $total;
-
 
 exit;
 
@@ -51,7 +44,18 @@ sub wanted {
 	my ($dev, $ino, $mode, $nlink, $uid, $gid);
 	(($dev, $ino, $mode, $nlink, $uid, $gid) = lstat($_)) &&
 		-f _ && /^.*\.h\z/s && push @files, $name &&
-		fgrep { print $_ if m/$query[0]/ } $name;
-		# fgrep { if (m/$query[0]/) { s/$query[0]/$'\033'[31m$&$'\033'[0m/gi; print $_; } } $name
+		map {
+			my $cur = $_;
+			fgrep {
+				$num-- && print $_ if m/$cur/;
+				exit if $num == 0;
+			} $name;
+		} @query;
+		# TODO: Get colors working
+		# map { my $cur = $_;
+		#         fgrep {
+		#                 s/$cur/$'\033'[31m$&$'\033'[0m/g && print $_ if m/$cur/
+		#         } $name
+		# } @query;
 }
 
